8b2e356fe55747bc5bda49b209125d82e1623a54
matti.comba <matti@comba.fr>
2024-02-16 15:56:45 +0100

    Add paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello les Mineurs! On est gentils ici, mais on se laisse pas marcher sur les platebandes! Voyons voir si cette sorcellerie fonctionne...</p>
+    <div>
+      <p class="text-4xl text-green-700 text-center py-20">Hello les Mineurs! On est gentils ici, mais on se laisse pas marcher sur les platebandes! Voyons voir si cette sorcellerie fonctionne...</p>
+      <p class="text-4xl text-green-700 text-center py-20">Hello les plus beaux !</p>
+    </div>
+    
   );
 }
 
