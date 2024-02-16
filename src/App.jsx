562bf899a64099225ea531485ce950b3265c9156
matti.comba <matti@comba.fr>
2024-02-16 15:26:22 +0100

    Modify text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello les Mineurs! On est gentils ici, mais on se laisse pas marcher sur les platebandes!</p>
+    <p class="text-4xl text-green-700 text-center py-20">Hello les Mineurs! On est gentils ici, mais on se laisse pas marcher sur les platebandes! Voyons voir si cette sorcellerie fonctionne...</p>
   );
 }
 
