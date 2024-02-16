c0946c03ea6980bd63e6ac92d7640251699c5f46
matti.comba <matti@comba.fr>
2024-02-16 16:12:57 +0100

    Mod try color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Le site de M.C.</h1>
+      <h1 class="text-4xl text-red-900 text-center py-20">Le site de M.C.</h1>
       <p class="text-4xl text-green-700 text-center py-20">Hello les Mineurs! On est gentils ici, mais on se laisse pas marcher sur les platebandes! Voyons voir si cette sorcellerie fonctionne...</p>
       <p class="text-4xl text-green-700 text-center py-20">Hello les plus beaux !</p>
     </div>
