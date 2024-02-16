bdeb0f6d57385bc54c9d89eeea0b2eb07d8056ea
matti.comba <matti@comba.fr>
2024-02-16 16:11:02 +0100

    Add h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <div>
+      <h1>Le site de M.C.</h1>
       <p class="text-4xl text-green-700 text-center py-20">Hello les Mineurs! On est gentils ici, mais on se laisse pas marcher sur les platebandes! Voyons voir si cette sorcellerie fonctionne...</p>
       <p class="text-4xl text-green-700 text-center py-20">Hello les plus beaux !</p>
     </div>
